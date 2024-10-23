72dad90d43b2fb8bd4a3fff98470f08688875ab2
FSD7-Boy <boyzasad01@gmail.com>
2024-10-23 13:27:30 +0700

    React Form Handling 1 Assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [displayText, setDisplayText] = useState("Greeting Message");
+
+  const onClickMsg = () => {
+    setDisplayText(inputValue);
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{displayText}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={onClickMsg}>Update text</button>
       </div>
     </div>
   );
